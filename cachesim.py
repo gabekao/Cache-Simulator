dfc5272e498413f9355f2965a67d65c31babde95
gabekao <38321687+gabekao@users.noreply.github.com>
2020-07-07 21:26:38 -0500

    Add files via upload

---
+++ cachesim.py
@@ -0,0 +1,51 @@
+#####################################
+# Cache Sim Project by Group 5      #
+# Members: Zack Ulloa,              #
+# Version: 1.0                      #
+# Date: 07/07/2020                  #
+# Class CS3853                      #
+#####################################
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class fileInfo(object):
+    filename = ""
+    cacheSize = 0
+    blockSize = 0
+    associativity = 0
+    replPol = ""
+    replPolStr = ""
+
+def processArgs():
+    currentFile = fileInfo()
+    if '-f' in sys.argv:
+        currentFile.filename = sys.argv[sys.argv.index('-f')+1]
+    if '-s' in sys.argv:
+        currentFile.cacheSize = sys.argv[sys.argv.index('-s')+1]
+    if '-b' in sys.argv:
+        currentFile.blockSize = sys.argv[sys.argv.index('-b')+1]
+    if '-a' in sys.argv:
+        currentFile.associativity = sys.argv[sys.argv.index('-a')+1]
+    if '-r' in sys.argv:
+        if 'RR' in sys.argv:
+            currentFile.replPol = 'RR'
+            currentFile.replPolStr = 'Round Robin'
+        if 'RND' in sys.argv:
+            currentFile.replPol = 'RND'
+            currentFile.replPolStr = 'Random'
+        if 'LRU' in sys.argv:
+            currentFile.replPol = 'LRU'
+            currentFile.replPolStr = 'Least Recently Used'
+    return currentFile
+
+# Print header
+print("Cache Simulator CS 3853 Summer 2020 Group #TBD")
+workingFile = processArgs()
+print("Trace File: " + workingFile.filename)
+print("***** Cache Input Parameters ****")
+print("Cache Size:\t\t" + workingFile.cacheSize + " KB")
+print("Block Size:\t\t" + workingFile.blockSize + " Bytes")
+print("Associativity:\t\t" + workingFile.associativity)
+print("Replacement Policy:\t" + workingFile.replPolStr)
